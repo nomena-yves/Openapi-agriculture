e73d9f9fdeb2ecf1bd8ab719e5c758cecbd42e0e
harena <tsutoru24@gmail.com>
2026-05-07 12:52:19 +0300

    add secure in the code with api-secure

---
+++ src/main/java/tsutsu/exam_final/config/ApiKeyFilter.java
@@ -0,0 +1,34 @@
+package tsutsu.exam_final.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class ApiKeyFilter extends OncePerRequestFilter {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String requestApiKey = request.getHeader("x-api-key");
+
+        if (requestApiKey == null || !requestApiKey.equals(apiKey)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Bad credentials");
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+}
